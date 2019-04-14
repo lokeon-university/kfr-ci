7566f2a182243a04c175193a5bb4eb5116ce6869
Rodrigo Sanabria <rodrigosanabria22@gmail.com>
2019-04-14 21:51:17 +0200

    created a monorepo for all things

---
+++ tg-bot/getrepos.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+var sess = session.Must(session.NewSessionWithOptions(session.Options{
+	Config: aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ID"), os.Getenv("AWS_SECRET"), ""),
+	},
+}))
+
+var dynClient = dynamodb.New(sess)
+var tableName = "GHTOKENS"
+
+type repositories struct {
+	ID            string
+	Name          string
+	SSHURL        string
+	NameWithOwner string
+}
+
+type userDB struct {
+	Token  string `json:"token"`
+	ChatID string `json:"chat_id"`
+	UserID string `json:"user_id"`
+}
+
+type query struct {
+	Viewer struct {
+		ID           string
+		Login        string
+		Repositories struct {
+			Nodes []repositories
+		} `graphql:"repositories(last: 20)"`
+	}
+}
+
+func runQuery(token string) (data query) {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubv4.NewClient(httpClient)
+	data = query{}
+	err := client.Query(context.Background(), &data, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	return
+}
+
+func getToken(user *tb.User) (string, error) {
+	result, err := dynClient.GetItem(&dynamodb.GetItemInput{
+		TableName: &tableName,
+		Key: map[string]*dynamodb.AttributeValue{
+			"user_id": {
+				S: aws.String(strconv.Itoa(user.ID)),
+			},
+		},
+	})
+	if err != nil {
+		log.Println(err)
+		log.Panicln("Error getting user from DB")
+	}
+	item := userDB{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+	if err != nil {
+		log.Panicln("Error Unmarshaling user from DB")
+	}
+	if item.Token == "" {
+		return "", errors.New("User is Not register")
+	}
+	return item.Token, nil
+}
+
+func (q *query) getReposList() [][]tb.InlineButton {
+	replyKeys := [][]tb.InlineButton{}
+	for _, repo := range q.Viewer.Repositories.Nodes {
+		replyKeys = append(replyKeys, []tb.InlineButton{{
+			Text: repo.NameWithOwner,
+			URL:  repo.SSHURL,
+			//TODO change URL for a Callback
+		},
+		})
+	}
+	return replyKeys
+}
+
+func (kfr *kfrBot) handleRepos() {
+	kfr.bot.Handle("/repos", func(m *tb.Message) {
+		token, err := getToken(m.Sender)
+		if err != nil {
+			kfr.bot.Send(m.Sender, "First call /auth")
+		} else {
+			repos := runQuery(token)
+			kfr.bot.Send(m.Sender, "Repositories", &tb.ReplyMarkup{
+				InlineKeyboard: repos.getReposList(),
+			})
+		}
+	})
+	log.Println("Handled Repos")
+}
---
+++ tg-bot/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type kfrBot struct {
+	bot *tb.Bot
+}
+
+func initBot() kfrBot {
+	bot, err := tb.NewBot(tb.Settings{
+		Token: os.Getenv("KFR_TELEGRAM"),
+		Poller: &tb.Webhook{
+			Listen: ":" + os.Getenv("PORT"),
+			Endpoint: &tb.WebhookEndpoint{
+				PublicURL: fmt.Sprintf("https://%s.herokuapp.com/%s", os.Getenv("HEROKU_APP_NAME"), os.Getenv("KFR_TELEGRAM")),
+			},
+		},
+	})
+	checkError(err)
+	return kfrBot{bot}
+}
+
+func (kfr *kfrBot) start() {
+	kfr.bot.Start()
+}
+
+func generateOauthURL(m *tb.Message) string {
+	baseURL, err := url.Parse("https://github.com/login/oauth/authorize")
+	checkError(err)
+	params := url.Values{}
+	params.Add("client_id", os.Getenv("GH_OCID"))
+	params.Add("scope", "admin:repo_hook repo read:org")
+	params.Add("redirect_uri", os.Getenv("GH_REDIRECT_URI"))
+	params.Add("state", fmt.Sprintf("%d %d", m.Sender.ID, m.Chat.ID))
+	baseURL.RawQuery = params.Encode()
+	return baseURL.String()
+}
+
+func (kfr *kfrBot) hadleAuth() {
+	kfr.bot.Handle("/auth", func(m *tb.Message) {
+		kfr.bot.Send(m.Sender, "GitHub", &tb.ReplyMarkup{
+			InlineKeyboard: [][]tb.InlineButton{{{
+				Text: "Confirm OAuth",
+				URL:  generateOauthURL(m)},
+			},
+			},
+		})
+	})
+	log.Println("Handled Auth")
+}
+
+func (kfr *kfrBot) handleStart() {
+	kfr.bot.Handle("/start", func(m *tb.Message) {
+		kfr.bot.Send(m.Sender, "Welcome to KFR CI")
+	})
+	log.Println("Handled Start")
+}
+
+func main() {
+	bot := initBot()
+	bot.handleStart()
+	bot.handleRepos()
+	bot.hadleAuth()
+	bot.start()
+}
