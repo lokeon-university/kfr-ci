1e2f55b81bc2d41a33c673f52769d3ae1bda3077
lokeon <kevin.lopezcala99@gmail.com>
2019-05-02 08:02:21 +0000

    Dockerfile go :penguin:

--- dockerfiles/go/docker-entrypoint.sh
+++ dockerfiles/go/docker-entrypoint.sh
@@ -5,34 +5,41 @@ source /etc/profile
 
 echo "<h3>Starting the build</h3>"
 
-echo "<h3>Adding SSH keys</h3>"
-mkdir -p /root/.ssh/ && cp -R .ssh/* "$_"
-chmod 600 /root/.ssh/* && \
-    ssh-keyscan github.com > /root/.ssh/known_hosts 
-echo ""
+#echo "<h3>Adding SSH keys</h3>"
+#mkdir -p /root/.ssh/ && cp -R .ssh/* "$_"
+#chmod 600 /root/.ssh/* &&
+#    ssh-keyscan github.com >/root/.ssh/known_hosts
+#echo
 
 echo "<h3>Checkout source code<h/3>"
 git clone $REPO_URL $REPO_NAME
 cd $REPO_NAME
 git checkout $REPO_BRANCH
-cd .
 echo
 
-# comprobamos si existe kfr.yml
-KFR_CONFIG_PRESENT = false
-KFR_CONFIG_FILE = ./kfr.yml
+# comprobamos si existe kfr.json
+KFR_CONFIG_PRESENT=false
+KFR_CONFIG_FILE=./kfr.json
 
-if [ -r ./kfr.yml ]; then
-    KFR_CONFIG_PRESENT = true
+if [ -r ./kfr.json ]; then
+    KFR_CONFIG_PRESENT=true
 fi
 
 echo "<h3>Dependencies</h3>"
 
+if $KFR_CONFIG_PRESENT; then
+    source <(cat .kfr-ci.json | jq -r '. | .env[]')
+fi
+
 go get -v ./...
 
-echo "<h3>Setup</h3>"
-if ! ($KFR_CONFIG_PRESENT ); then
-    go build 
+echo "<h3>Build</h3>"
+if $KFR_CONFIG_PRESENT; then
+    cat .kfr-ci.json | jq -r '. | .build[]' | bash
+fi
+
+if !($KFR_CONFIG_PRESENT); then
+    go build
 fi
 
 exec "$@"
