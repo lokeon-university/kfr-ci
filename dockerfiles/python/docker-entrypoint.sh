d52793db3dc8ac5be114ff1a2359ec12cb814e66
rafaroman18 <rafaroman182@gmail.com>
2019-05-03 18:03:01 +0200

    python docker image

--- dockerfiles/python/docker-entrypoint.sh
+++ dockerfiles/python/docker-entrypoint.sh
@@ -7,34 +7,31 @@ echo "<h3>Checkout source code<h/3>"
 git clone $REPO_URL $REPO_NAME
 cd $REPO_NAME
 git checkout $REPO_BRANCH
-echo
+cd .
+echo ""
 
 # comprobamos si existe kfr.json
 KFR_CONFIG_PRESENT=false
-KFR_CONFIG_FILE=./kfr.json
+KFR_CONFIG_FILE=./.kfr.json
 
 REQ_PRESENT=true
 REQ_FILE=./requirements.txt
 
-if [ -r ./kfr.json ]; then
+if [ -r ./.kfr.json ]; then
     echo "Found .kfr.json file." 1>&2
     KFR_CONFIG_PRESENT=true
 fi
 
 echo "<h3>Dependencies</h3>"
 
-if ! [ -r ./requirements.txt]; then
-    echo "ERROR. FALTA requirements.txt" 1>&2
+if ![ -r ./requirements.txt]; then
+    echo "ERROR. FALTA 'requirements.txt' " 1>&2
     exit 1
-fi
-
-if ($KFR_CONFIG_PRESENT && $REQ_PRESENT); then
-    pip3 install -r requirements.txt
 fi
 
 echo "<h3>Build</h3>"
 
-if $KFR_CONFIG_PRESENT; then
+if ($KFR_CONFIG_PRESENT && $REQ_PRESENT); then
     cat .kfr-ci.json | jq -r '. | .build[]' | bash
 fi
 
